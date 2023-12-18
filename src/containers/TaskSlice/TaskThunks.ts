dcfd933216a6a2d3465a57db6082a0be5fabc87b
Oleg Medvedev <meddway@gmail.com>
2023-12-19 02:36:52 +0600

    Добавлены асинхронные функции TaskThunks для выполнения запросов к API по удалению, добавлению переключению

--- src/containers/TaskSlice/TaskThunks.ts
+++ src/containers/TaskSlice/TaskThunks.ts
@@ -1,8 +1,79 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import axiosApi from "../../axiosApi";
+import {AppDispatch, RootState} from "../../app/store";
 
-export const fetchTasks
 
-export const addTask
+interface Task {
+  id: string;
+  title: string;
+  status: boolean;
+}
 
-export const toggleTaskStatus
+type AsyncThunkConfig = {
+  state: RootState;
+  rejectValue: string;
+  dispatch: AppDispatch;
+};
 
-export const deleteTask
+export const fetchTasks = createAsyncThunk<Task[], void, AsyncThunkConfig>(
+  'tasks/fetchTasks',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axiosApi.get<{ [key: string]: Task }>('/tasks.json');
+      const tasksArray = Object.entries(response.data || {}).map(([id, task]) => ({
+        id,
+        title: task.title,
+        status: task.status,
+      }));
+      return tasksArray || [];
+    } catch (error) {
+      return rejectWithValue('Failed to fetch tasks');
+    }
+  }
+);
+
+export const addTask = createAsyncThunk<Task, string, AsyncThunkConfig>(
+  'tasks/addTask',
+  async (title, { rejectWithValue }) => {
+    try {
+      const response = await axiosApi.post<Task>('/tasks.json', { title, status: false });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue('Failed to add task');
+    }
+  }
+);
+
+export const toggleTaskStatus = createAsyncThunk<Task, string, AsyncThunkConfig>(
+  'tasks/toggleTaskStatus',
+  async (id, { rejectWithValue, getState }) => {
+    const { tasks } = getState().tasks;
+    const task = tasks.find((t) => t.id === id);
+
+    if (!task) {
+      return rejectWithValue('Task not found');
+    }
+
+    try {
+      const response = await axiosApi.put<Task>(`/tasks/${id}.json`, {
+        ...task,
+        status: !task.status,
+      });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue('Failed to toggle task status');
+    }
+  }
+);
+
+export const deleteTask = createAsyncThunk<void, string, AsyncThunkConfig>(
+  'tasks/deleteTask',
+  async (id, { rejectWithValue, dispatch }) => {
+    try {
+      await axiosApi.delete(`/tasks/${id}.json`);
+      await dispatch(fetchTasks());
+    } catch (error) {
+      return rejectWithValue('Failed to delete task');
+    }
+  }
+);
